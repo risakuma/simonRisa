3cf13f8a6262129e3ebd48b2a1eaff2146e8b1ae
Moo <Risa@192.168.2.6>
2017-01-08 21:43:46 -0500

    classes

---
+++ SimonRisa/src/simon/Move.java
@@ -0,0 +1,15 @@
+package simon;
+
+public class Move implements MoveInterfaceRisa {
+
+	public Move() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ButtonInterfaceRisa getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
